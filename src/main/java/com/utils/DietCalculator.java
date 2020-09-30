ce807fb555f6e92f43a5f5396ff0d443ee304100
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-09-30 12:23:54 +0900

    array를 List로 변경 for문을 for each문으로 수정

--- src/main/java/com/utils/DietCalculator.java
+++ src/main/java/com/utils/DietCalculator.java
@@ -1,18 +1,19 @@
 package com.utils;
 
 import com.model.DietDTO;
+import com.model.MenuDTO;
 
 import java.util.*;
 
 public class DietCalculator {
-    public double calculateScoreOfDiet(DietDTO dietDTO){
+    public double calculateScoreOfDiet(DietDTO dietDTO) {
         Set<String> colorScore = new HashSet<>();
         colorScore.add(dietDTO.getRice().getColor());
         colorScore.add(dietDTO.getSoup().getColor());
         colorScore.add(dietDTO.getKimchi().getColor());
         colorScore.add(dietDTO.getMain().getColor());
-        for(int i=0;i<dietDTO.getSideList().size();i++){
-            colorScore.add(dietDTO.getSideList().get(i).getColor());
+        for (MenuDTO side : dietDTO.getSideList()) {
+            colorScore.add(side.getColor());
         }
 
         Set<Integer> ingredientScore = new HashSet<>();
@@ -20,8 +21,8 @@ public class DietCalculator {
         ingredientScore.add(dietDTO.getSoup().getIngredient());
         ingredientScore.add(dietDTO.getKimchi().getIngredient());
         ingredientScore.add(dietDTO.getMain().getIngredient());
-        for(int i=0;i<dietDTO.getSideList().size();i++){
-            ingredientScore.add(dietDTO.getSideList().get(i).getIngredient());
+        for (MenuDTO side : dietDTO.getSideList()) {
+            ingredientScore.add(side.getIngredient());
         }
 
         Set<Integer> recipeScore = new HashSet<>();
@@ -29,84 +30,82 @@ public class DietCalculator {
         recipeScore.add(dietDTO.getSoup().getRecipe());
         recipeScore.add(dietDTO.getKimchi().getRecipe());
         recipeScore.add(dietDTO.getMain().getRecipe());
-        for(int i=0;i<dietDTO.getSideList().size();i++){
-            recipeScore.add(dietDTO.getSideList().get(i).getRecipe());
+        for (MenuDTO side : dietDTO.getSideList()) {
+            recipeScore.add(side.getRecipe());
         }
 
-        String[] country = new String[6];
-        country[0] = dietDTO.getRice().getCountry();
-        country[1] = dietDTO.getSoup().getCountry();
-        country[2] = dietDTO.getKimchi().getCountry();
-        country[3]= dietDTO.getMain().getCountry();
-        country[4] = dietDTO.getSideList().get(0).getCountry();
-        country[5] = dietDTO.getSideList().get(1).getCountry();
+        List<String> countryList = new ArrayList<>();
+        countryList.add(dietDTO.getRice().getCountry());
+        countryList.add(dietDTO.getSoup().getCountry());
+        countryList.add(dietDTO.getKimchi().getCountry());
+        countryList.add(dietDTO.getMain().getCountry());
+        countryList.add(dietDTO.getSideList().get(0).getCountry());
+        countryList.add(dietDTO.getSideList().get(1).getCountry());
 
         Map<String, Integer> countryOfMap = new HashMap<>();
-        for(int i=0;i<country.length;i++) {
-            if (countryOfMap.containsKey(country[i])){
-                countryOfMap.put(country[i],countryOfMap.get(country[i])+1);
-            } else{
-                countryOfMap.put(country[i],1);
+        for (String country : countryList) {
+            if (countryOfMap.containsKey(country)) {
+                countryOfMap.put(country, countryOfMap.get(country) + 1);
+            } else {
+                countryOfMap.put(country, 1);
             }
         }
-        double countryMax =0;
+        double countryMax = 0;
         double countryNum = countryOfMap.keySet().size();
-        for(String key : countryOfMap.keySet()) {
+        for (String key : countryOfMap.keySet()) {
             if (countryMax < countryOfMap.get(key)) {
                 countryMax = countryOfMap.get(key);
             }
         }
         double countryScore = countryMax / countryNum;
-        return colorScore.size()+ingredientScore.size()+recipeScore.size()+countryScore;
+        return colorScore.size() + ingredientScore.size() + recipeScore.size() + countryScore;
     }
 
-    public double calculateScoreOfDietList(List<DietDTO> dietDTOList){
+    public double calculateScoreOfDietList(List<DietDTO> dietDTOList) {
 
-        int[] ingredient = new int[dietDTOList.size()];
-
-        for(int i=0;i<dietDTOList.size();i++){
-            ingredient[i]=dietDTOList.get(i).getMain().getIngredient();
+        List<Integer> ingredientList = new ArrayList<>();
+        for (DietDTO dietDTO : dietDTOList) {
+            ingredientList.add(dietDTO.getMain().getIngredient());
         }
-        Map<Integer,Integer> ingredientOfMap = new HashMap<>();
-        for(int i=0;i<ingredient.length;i++){
-            if(ingredientOfMap.containsKey(ingredient[i])){
-                ingredientOfMap.put(ingredient[i],ingredientOfMap.get(ingredient[i])+1);
-            }else{
-                ingredientOfMap.put(ingredient[i],1);
+
+        Map<Integer, Integer> ingredientOfMap = new HashMap<>();
+        for (int ingredient : ingredientList) {
+            if (ingredientOfMap.containsKey(ingredient)) {
+                ingredientOfMap.put(ingredient, ingredientOfMap.get(ingredient) + 1);
+            } else {
+                ingredientOfMap.put(ingredient, 1);
             }
         }
-        double ingredientMax =0;
+        double ingredientMax = 0;
         double ingredientNum = ingredientOfMap.keySet().size();
-        for(int key : ingredientOfMap.keySet()) {
+        for (int key : ingredientOfMap.keySet()) {
             if (ingredientMax < ingredientOfMap.get(key)) {
                 ingredientMax = ingredientOfMap.get(key);
             }
         }
         double ingredientScore = ingredientMax / ingredientNum;
 
-
-
-        String[] country = new String[dietDTOList.size()];
-        for(int i=0;i<dietDTOList.size();i++){
-            country[i]=dietDTOList.get(i).getMain().getCountry();
+        List<String> countryList = new ArrayList<>();
+        for (DietDTO dietDTO : dietDTOList) {
+            countryList.add(dietDTO.getMain().getCountry());
         }
         Map<String, Integer> countryOfMap = new HashMap<>();
-        for(int i=0;i<country.length;i++) {
-            if (countryOfMap.containsKey(country[i])){
-                countryOfMap.put(country[i],countryOfMap.get(country[i])+1);
-            } else{
-                countryOfMap.put(country[i],1);
+        for (String country : countryList) {
+            if (countryOfMap.containsKey(country)) {
+                countryOfMap.put(country, countryOfMap.get(country) + 1);
+            } else {
+                countryOfMap.put(country, 1);
             }
         }
-        double countryMax =0;
+        double countryMax = 0;
         double countryNum = countryOfMap.keySet().size();
-        for(String key : countryOfMap.keySet()) {
+        for (String key : countryOfMap.keySet()) {
             if (countryMax < countryOfMap.get(key)) {
                 countryMax = countryOfMap.get(key);
             }
         }
         double countryScore = countryMax / countryNum;
 
-        return ingredientScore+countryScore;
+        return ingredientScore + countryScore;
     }
 }
