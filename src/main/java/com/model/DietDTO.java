393cf50b156e63169720081a9143162ed03ce2d8
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-10-01 20:00:43 +0900

    DietDTO 수정

--- src/main/java/com/model/DietDTO.java
+++ src/main/java/com/model/DietDTO.java
@@ -4,16 +4,48 @@ package com.model;
 import java.util.List;
 
 public class DietDTO {
-    private List<MenuDTO> menuList;
+    private final MenuDTO rice;
+    private final MenuDTO soup;
+    private final MenuDTO kimchi;
+    private final MenuDTO main;
+    private final List<MenuDTO> sideList;
 
-    public DietDTO(List<MenuDTO> menuList){
-        this.menuList = menuList;
-    }
-    public List<MenuDTO> getMenuList() {
-        return menuList;
+    public DietDTO(MenuDTO rice, MenuDTO soup, MenuDTO kimchi, MenuDTO main, List<MenuDTO> sideList) {
+        this.rice = rice;
+        this.soup = soup;
+        this.kimchi = kimchi;
+        this.main = main;
+        this.sideList = sideList;
     }
 
-    public void setMenuList(List<MenuDTO> menuList) {
-        this.menuList = menuList;
+    public MenuDTO getRice() {
+        return rice;
+    }
+
+    public MenuDTO getSoup() {
+        return soup;
+    }
+
+    public MenuDTO getKimchi() {
+        return kimchi;
+    }
+
+    public MenuDTO getMain() {
+        return main;
+    }
+
+    public List<MenuDTO> getSideList() {
+        return sideList;
+    }
+
+    @Override
+    public String toString() {
+        return "DietDTO{" +
+                "rice=" + rice +
+                ", soup=" + soup +
+                ", kimchi=" + kimchi +
+                ", main=" + main +
+                ", sideList=" + sideList +
+                '}';
     }
 }
