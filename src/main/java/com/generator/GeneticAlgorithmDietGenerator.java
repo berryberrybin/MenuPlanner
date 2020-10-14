4bab501b27ed5b3e3846971b6506740517ac397f
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-10-14 22:14:12 +0900

    [#14] Genetic Algorithm 구현 클래스 생성

---
+++ src/main/java/com/generator/GeneticAlgorithmDietGenerator.java
@@ -0,0 +1,38 @@
+package com.generator;
+
+
+import com.manager.MenuManager;
+import com.model.DietTable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneticAlgorithmDietGenerator implements DietGenerator {
+    private final MenuManager menuManager;
+
+    public GeneticAlgorithmDietGenerator(MenuManager menuManager) {
+        this.menuManager = menuManager;
+    }
+
+    @Override
+    public DietTable generate(int n) {
+        List<DietTable> geneDiets = new ArrayList<>();
+        // 초기 유전자 세트 생성
+        RandomDietGenerator randomDietGenerator = new RandomDietGenerator(menuManager);
+        while(geneDiets.size()<100) {
+            geneDiets.add(randomDietGenerator.generate(n));
+        }
+
+        // TODO score 기준 정렬
+
+        // TODO elite 유전자 추출
+
+        // TODO 돌연변이 유전자 생성
+
+        // TODO 새로운 유전자 채워넣기
+
+        // TODO 가장 우월한 유전자 반환
+        return geneDiets.get(0);
+    }
+
+}
